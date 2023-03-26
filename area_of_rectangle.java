7893fc1b68b6cfd05f6dd22f004c9e0ee2f2c99d
Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2023-03-27 01:12:36 +0530

    Add files via upload

---
+++ area_of_rectangle.java
@@ -0,0 +1,10 @@
+public class area_of_rectangle {
+  //using local variable
+    public static void main(String args[]){
+        int length = 80;
+	int width = 40;
+        int area=length*width;
+        System.out.println("Area of Rectangle = "+area);
+    
+}
+}
---
+++ simple_interest.java
@@ -0,0 +1,13 @@
+public class simple_interest {
+
+        //using local variable
+
+    public static void main(String args[]){
+	float initial = 12600;
+	float interest = 6;
+	float tenure = 5;
+	float si = (initial * interest * tenure) / 100;
+        System.out.println("Total Simple interest after 5 years =  "+si);
+    
+}
+}
