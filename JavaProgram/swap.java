13f493ae588347b5597ed8a4da2385be39ce7254
Mohammad Adil Rasheed <er.adilrasheed@gmail.com>
2024-05-24 17:37:53 +0530

    swap

---
+++ JavaProgram/swap.java
@@ -0,0 +1,22 @@
+// code to swap two integers
+import java.util.Scanner;
+public class swap {
+  // function to swap two integers using third variable
+  static int swapin(int a, int b){
+    int temp=a; a=b; b=temp;
+    System.out.println("First num = "+a+", Second num = "+b+" (after function call)");
+    return 0;
+  }
+  public static void main(String[] args){
+    // local variables
+    int num1,num2;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter First Number");
+    num1 = sc.nextInt();
+    System.out.println("Enter Second Number");
+    num2 = sc.nextInt();
+    System.out.println("First num = "+num1+", Second num = "+num2+" (before function call)");
+    swapin(num1,num2);
+    sc.close();
+  }
+}
